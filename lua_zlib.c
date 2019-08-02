f3293a813c3160b3aa27d80e7225847ea583185e
David Guerrero <david.guerrero@prezi.com>
2019-08-02 14:37:16 +0200

    Trying pushcfunction

--- lua_zlib.c
+++ lua_zlib.c
@@ -1261,7 +1261,7 @@ LUALIB_API int luaopen_zlib(lua_State * const L) {
     lz_create_inflate_mt(L);
 
     lua_newtable(L);
-    luaL_setfuncs(L, zlib_functions, 0);
+    lua_pushcfunction(L, zlib_functions);
 
     SETINT("BEST_SPEED", Z_BEST_SPEED);
     SETINT("BEST_COMPRESSION", Z_BEST_COMPRESSION);
