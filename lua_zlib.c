dedc2c36eb25c4fd3e61a68891ca53eac9b33e47
David Guerrero <david.guerrero@prezi.com>
2019-08-02 14:02:43 +0200

    Using luaL_setfuncs correctly

--- lua_zlib.c
+++ lua_zlib.c
@@ -1260,7 +1260,7 @@ LUALIB_API int luaopen_zlib(lua_State * const L) {
     lz_create_deflate_mt(L);
     lz_create_inflate_mt(L);
 
-    luaL_setfuncs(L, "zlib", zlib_functions);
+    luaL_setfuncs(L, zlib_functions, 0);
 
     SETINT("BEST_SPEED", Z_BEST_SPEED);
     SETINT("BEST_COMPRESSION", Z_BEST_COMPRESSION);
